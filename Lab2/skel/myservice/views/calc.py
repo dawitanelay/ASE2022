0aebc33dbd686ea0f62337802e8fe59e7d72c3f0
dawitanelay <dawitanelay@gmail.com>
2021-10-12 16:51:49 +0200

    sub and mult

--- Lab2/skel/myservice/views/calc.py
+++ Lab2/skel/myservice/views/calc.py
@@ -31,4 +31,33 @@ def devide():
         m -= n
         result += 1
         result = - result if negativeResult else result
-    return jsonify({'result':str(result)}) 
+    return jsonify({'result':str(result)})
+
+@calc.route('/multiply', methods=['GET'])
+def multiply():
+    m = int(request.args.get('m'))
+    n = int(request.args.get('n'))
+    r=0
+    i=0
+    while i<abs(n):
+        r += abs(m)
+        i += 1
+    if ((m<0 and n>0) or (m>0 and n<0)):
+        result =- r
+    else:
+            result = r
+    return jsonify({'result':str(result)})
+
+
+@calc.route('/subtract', methods=['GET'])
+def subtract():
+    m = int(request.args.get('m'))
+    n = int(request.args.get('n'))
+    r = m
+    if n < 0:
+        for i in range(abs(n)):
+            r +=1
+    else:
+        for i in range(abs(n)):
+            r -=1
+    return  jsonify({'result':str(r)}) 
